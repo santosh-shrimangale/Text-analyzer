d2b51cde5443f9ae55d3cb980801aa4adaaf90c4
santosh-shrimangale <santoshshrimangale01@gmaiil.com>
2024-03-13 00:14:01 +0530

    Added words and charactor functionality

--- src/components/TextAnalyzer/TextAnalyzer.js
+++ src/components/TextAnalyzer/TextAnalyzer.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./TextAnalyzer.css"
 
 function TextAnalyzer() {
+    const [word, setWord] = useState(0);
+    const [character, setCharacter] = useState(0);
+    const [sentence, setSentence] = useState(0);
+
     const handleChangetext = (e) => {
-        console.log(e.target.value);
+        let text = e.target.value;
+        // for word count
+        let res = text.trim().split(" ");
+        setWord(res.length);
+        //for character count
+        let res1 = text.trim().split("").filter(char => char !== " ");
+        setCharacter(res1.length);
+        // let res2 = text.trim().split("").filter(char => char !== ".");
+        // console.log(res2);
+        // setSentence(res2.length);
+
+
     }
     return (
         <div>
             <div className="resultBar">
                 <div className="words">
                     <span>Words</span>
-                    <span className='valueBar'>0</span>
+                    <span className='valueBar'>{word}</span>
                 </div>
                 <div className="characters">
                     <span>Characters</span>
-                    <span className='valueBar'>0</span>
+                    <span className='valueBar'>{character}</span>
                 </div>
                 <div className="sentence">
                     <span>Sentence</span>
-                    <span className='valueBar'>0</span>
+                    <span className='valueBar'>{sentence}</span>
                 </div>
                 <div className="paragraph">
                     <span>Paragraph</span>
